632d5f351fa42c3e7ad3a2b9450dc6ccc8ee0ab5
SD-Kalsky <sdkalsky@gmail.com>
2024-11-02 04:55:52 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "controller.h"
+using namespace std;
+
+int main()
+{
+    start();
+
+    // Stack a;
+    // srand(time(0));
+    // int i=0;
+    // while(i<10)
+    // {
+    //     a.push(rand()%100);
+    //     i++;
+    // }
+    // a.print();
+    // println();
+
+    // i=0;
+    // while(i<10)
+    // {
+    //     if (rand()%2==0) a.push(rand()%100);
+    //     else a.pop();
+    //     i++;
+    // }
+    // a.print();
+
+    // Заполнить 10 случайных элементов
+    // Цикл 30 повторений и в каждом повторении сделать следующее: генераия 0 или 1 0 - удалить 1 - добавить случайное число
+    
+
+    return 0;
+}
