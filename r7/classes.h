ce375bf0aff58b1281c56f52e60055327aaf0de0
SD-Kalsky <sdkalsky@gmail.com>
2025-01-10 06:35:06 +0700

    Yeah

--- r7/classes.h
+++ r7/classes.h
@@ -1,39 +1,7 @@
 #include <iostream>
+#include <vector>
 // #include <cmath>
 using namespace std;
-
-int naivMod(int x, int a, int m){
-    int y=1, i=0;
-    while(i<x){
-        y *= a;
-        i++;
-    }
-    cout<<"y="<<y<<" ";
-    y = y % m;
-    return y;
-}
-
-int fastMod(int x, int a, int m){
-    int y=1;
-    int i=1, p=1;
-    while(x>0){
-        y*=( x % 2 > 0 ?  a : 1 );
-        a *= a;
-        x /= 2;
-    }
-    cout<<"y="<<y<<" ";
-    y = y % m;
-    return y;
-}
-
-int reModBruteForce(int y, int a, int m){
-    int x=0;
-    int i=1, p=1;
-    while(x%m==y){
-        y *= a;
-    }
-    return y;
-}
 
 int toBinary(int n){
     int a=0;
@@ -45,6 +13,76 @@ int toBinary(int n){
     }
     return a;
 }
+
+bool isSimple(int a){
+    bool b=true;
+    int i=2;
+    while(i<a){
+        if ( a % i == 0 ) {b=false; break;}  
+        i++;
+    }
+    return b;
+}
+
+int toSimple(int a){
+    int i=0;
+    while(i<a){
+        if ( isSimple( a += i )) break; 
+        i++;
+    }
+    return a;
+}
+
+int naivMod(int x, int a, int p){
+    int y=1, i=0;
+    while(i<x){
+        y *= a;
+        i++;
+    }
+    cout<<"y="<<y<<" ";
+    y = y % p;
+    return y;
+}
+
+int fastMod(int x, int a, int p){
+    int y=1;
+    while(x>0){
+        y*=( x % 2 > 0 ?  a : 1 );
+        a *= a;
+        x /= 2;
+    }
+    y = y % p;
+    return y;
+}
+
+int reModBruteForce(int y, int a, int p){
+    int x=0;
+    bool b=false;
+    while( x < p ){
+        if(  y == fastMod( x, a, p ) ) {b=true; break;}
+        x++;
+    }
+    return x *= b;
+}
+
+vector<int> makeKeys(int g, int p){
+    p = toSimple(p);
+    g = toSimple(g);
+    int q;
+    q = ( p - 1 )/ 2;
+    if( 1 == fastMod(q, g, p) || g <= 1 || p - 1 <= g ){
+        g = p - 2;
+        g = toSimple(g);
+        while( 1 == fastMod(q, g, p) ){
+            g = toSimple(g);
+        }
+    }
+    vector<int> keys{g,p};
+    return keys;
+}
+
+
+
 class DaClass{
 
 };
--- r7/main.cpp
+++ r7/main.cpp
@@ -3,14 +3,28 @@
 
 using namespace std;
 
+void transferAB(){
+    int g, p, a, b;
+
+}
+
+
 int main()
 {
-    int n=5, a=5, m=3, c;
+    int x=1024, a=5, y;
     int i=5;
-    c=naivMod(n, a, m);
-    cout << c << endl;
-    c=fastMod(n, a, m);
-    cout << c << endl;
 
+    int g = 3, p = 23;
+
+    vector<int> keys;
+    
+    keys = makeKeys( g, p );
+
+    cout << keys[0] << " " << keys[1];
+
+    // y=fastMod(x, a, p);
+    // cout << "For x=" << x << " we got y=" <<y << endl;
+    // x=reModBruteForce(y,a,p);
+    // cout << "For y=" << y << " we got x=" <<x << endl;
     return 0;
 }
